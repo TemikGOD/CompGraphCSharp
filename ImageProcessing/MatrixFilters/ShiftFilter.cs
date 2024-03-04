6b91aea738916b4c9e2d1b91a4aeef46a4ed164b
Nanuzer0 <ivragov3@gmail.com>
2024-03-04 19:47:04 +0300

    ShiftFilter

---
+++ ImageProcessing/MatrixFilters/ShiftFilter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageProcessing.MatrixFilters
+{
+    internal class ShiftFilter : Filter
+    {
+        int ShiftX, ShiftY;
+        public ShiftFilter(int shiftX, int shiftY)
+        {
+            ShiftX = shiftX;
+            ShiftY = shiftY;
+        }
+
+        protected override byte[] CalculateNewPixelColor(byte[] buffer, int x, int y, int width, int depth)
+        {
+            var offset = new int[depth];
+            var resultColor = new byte[depth];
+            if (x + ShiftX < width - 1 && y + ShiftY < buffer.Length / depth / width - 1)
+            {
+                for (var i = 0; i < depth; i++)
+                    offset[i] = (((y + ShiftY) * width) + x + ShiftX) * depth;
+                for (int i = 0; i < depth; i++)
+                    resultColor[i] = buffer[offset[i]];
+            }
+            else
+            {
+                for (int i = 0; i < depth; i++)
+                    resultColor[i] = 0;
+            }
+            return resultColor;
+        }
+    }
+}
