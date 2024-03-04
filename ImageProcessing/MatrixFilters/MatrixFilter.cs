c2173907f9b58f4e70adf3f9c7e07b3564b05c53
TemikGOD <denisov4tamik@outlook.com>
2024-03-04 19:00:09 +0300

    The work of processImage has been fixed

--- ImageProcessing/MatrixFilters/MatrixFilter.cs
+++ ImageProcessing/MatrixFilters/MatrixFilter.cs
@@ -15,27 +15,26 @@ namespace ImageProcessing
             this.kernel = kernel;
         }
 
-        protected override void CalculateNewPixelColor(byte[] buffer, int x, int y, int width, int depth)
+        protected override byte[] CalculateNewPixelColor(byte[] buffer, int x, int y, int width, int depth)
         {
             int radiusX = kernel.GetLength(0) / 2;
             int radiusY = kernel.GetLength(1) / 2;
-            float resultR = 0;
-            float resultG = 0;
-            float resultB = 0;
+            var result = new float[depth];
             for (int l = -radiusY; l <= radiusY; l++)
                 for (int k = -radiusX; k <= radiusX; k++)
                 {
                     int idX = Clamp(x + k, 0, width - 1);
                     int idY = Clamp(y + l, 0, buffer.Length / depth / width - 1);
-                    var offsetNeighbor = ((idY * width) + idX) * depth;
-                    resultR += buffer[offsetNeighbor + 0] * kernel[k + radiusX, l + radiusY];
-                    resultG += buffer[offsetNeighbor + 1] * kernel[k + radiusX, l + radiusY];
-                    resultB += buffer[offsetNeighbor + 2] * kernel[k + radiusX, l + radiusY];
+                    var offsetNeighbor = CalculateOffset(idX, idY, width, depth);
+                    for (int i = 0; i < depth; i++) {
+                        result[i] += buffer[offsetNeighbor[0]] * kernel[k + radiusX, l + radiusY];
+                    }
                 }
-            var offset = ((y * width) + x) * depth;
-            buffer[offset + 0] = (byte)Clamp((int)resultR, 0, 255);
-            buffer[offset + 1] = (byte)Clamp((int)resultG, 0, 255);
-            buffer[offset + 2] = (byte)Clamp((int)resultB, 0, 255);
+            var offset = CalculateOffset(x, y, width, depth);
+            var resultColor = new byte[depth];
+            for (int i = 0; i < depth; i++)
+                resultColor[i] = (byte)Clamp((int)result[i], 0, 255); 
+            return resultColor;
         }
     }
 }
