00f8c24e099b6af7f5a9fd1c6b7db568b7b3dfbe
Nanuzer0 <ivragov3@gmail.com>
2024-03-04 21:24:15 +0300

    Temp3

--- ImageProcessing/DotetFilters/Turn.cs
+++ ImageProcessing/DotetFilters/Turn.cs
@@ -8,10 +8,33 @@ namespace ImageProcessing.DotetFilters
 {
     internal class Turn : Filter
     {
-        
+        protected double deg;
+        protected int x0, y0;
+
+        public Turn(double deg, int x0, int y0)
+        {
+            this.deg = deg;
+            this.x0 = x0;
+            this.y0 = y0;
+        }
+
         protected override byte[] CalculateNewPixelColor(byte[] buffer, int x, int y, int width, int depth)
         {
-            
+            var offset = new int[depth];
+            var resultColor = new byte[depth];
+            if (x + ShiftX < width - 1 && y + ShiftY < buffer.Length / depth / width - 1)
+            {
+                for (var i = 0; i < depth; i++)
+                    offset[i] = (((y + ShiftY) * width) + x + ShiftX) * depth + i;
+                for (int i = 0; i < depth; i++)
+                    resultColor[i] = buffer[offset[i]];
+            }
+            else
+            {
+                for (int i = 0; i < depth; i++)
+                    resultColor[i] = 0;
+            }
+            return resultColor;
         }
     }
 }
